fa97b472ce3cf5caf6210c4760be90995cf77bfa
minji7901 <88637782+minji7901@users.noreply.github.com>
2024-10-22 11:41:53 +0900

    test(listItem, style)
    리팩토링 및 style 함수 추가

--- js/movieList.js
+++ js/movieList.js
@@ -12,20 +12,26 @@ function createListItem(movie) {
   const vote = movie.vote_average.toFixed(1);
   const day = movie.release_date.slice(0, 4);
   listItem.innerHTML = `
-    <div class="section__list-item">
-      <div class="section__list-imgbox">
-        <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="section__list-img" />
-        <span class="section__list-vote">⭐${vote}</span>
-      </div>
-      <p class="section__list-title">${movie.title}</p>
-      <p class="section__list-day">${day}</p>
+    <div class="section__list-imgbox">
+      <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="section__list-img" />
+      <span class="section__list-vote">⭐${vote}</span>
     </div>
+    <p class="section__list-title">${movie.title}</p>
+    <p class="section__list-day">${day}</p>
   `;
-  listItem
-    .querySelector(".section__list-item")
-    .addEventListener("click", () => {
-      document.querySelector("body").classList.add("hidden");
-      getMovieData(false, movie.id);
+  listItem.addEventListener("click", () => {
+    document.querySelector("body").classList.add("hidden");
+    getMovieData(false, movie.id);
+  });
+  listItem.addEventListener("mouseenter", () => {
+    document.querySelectorAll(".section__list li").forEach((item) => {
+      item.style.opacity = "0.3";
     });
+  });
+  listItem.addEventListener("mouseleave", () => {
+    document.querySelectorAll(".section__list li").forEach((item) => {
+      item.style.opacity = "1";
+    });
+  });
   return listItem;
 }
